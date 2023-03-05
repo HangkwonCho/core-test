5d6334bac236c98a0590e956131504a575ef7d43
HangkwonCho <vatican0411@gmail.com>
2023-03-05 23:39:02 +0900

    update

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,4 +10,7 @@ import org.springframework.context.annotation.FilterType;
         @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+        // @ComponentScan 으로 하는 방법
+        // @Component 가 붙은 클래스를 스캔하여 bean 컨테이너에 등록
 }
