8b5003f339e24b0a03bcfeeef024600d6e23ce11
HangkwonCho <vatican0411@gmail.com>
2023-03-07 17:44:49 +0900

    Update

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -11,6 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrototypeTest {
 
+    // 프로토타입 빈의 특징
+    // 1. 스프링 컨테어니에 요청할 때 마다 새로 생성한다.
+    // 2. 스프링 컨테어니는 프로토타입 빈의 생성과 의존과계 주입 그리고 초기화까지만 관여한다.
+    // 3. 종료 메서드가 호출되지 않는다.
+    // 4. 프로토타입 빈은 프로토타입 빈을 조회한 클라이언트가 관리해야 한다. 종료 메서드에 대한 호출도 클라이언트가 직접 해야한다.
+
     @Test
     void prototypeBeanFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
@@ -25,6 +31,9 @@ public class PrototypeTest {
         System.out.println("prototypeBean2 = " + prototypeBean2);
 
         assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
 
         ac.close();
     }
